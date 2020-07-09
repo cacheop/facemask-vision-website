7bbda6a97b1db4510eb3bc1a8bf5934cbfbe43b1
Alberto Escarlate <alba@cacheop.com>
2020-07-09 18:11:59 -0400

    all files

---
+++ aws_site/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    gcp_email: '',
+    gcp_clientId: '',
+    gcp_clientSecret: '',
+    gcp_refreshToken: '',
+
+    aws_region: 'ap-southeast-1',
+    aws_accessKeyId:'',
+    aws_secretAccessKey:'',
+
+    email_from: 'Face Mask Vision <no-reply@cacheop.com>',
+    email_subject_confirmation: '[Face Mask Vision] Thanks for your inquiry',
+    email_body: 'We will be in touch'
+};
